79cc299f5825ef7239aef29b846abb2e70bd298a
Daniel <danielalves672@gmail.com>
2025-03-04 15:48:20 -0300

    menu

--- js/select-open.js
+++ js/select-open.js
@@ -35,3 +35,26 @@ function eventosGaleria(img) {
 galeria.forEach(eventosGaleria);
 
 console.log(photosContainer);
+
+//menu
+
+const btnMobile = document.querySelector(".toggle");
+const menuMobile = document.querySelector(".slide");
+const closeMenu = document.querySelector(".close-menu");
+
+btnMobile.onclick = function () {
+  menuMobile.classList.add("ativo");
+};
+
+closeMenu.onclick = function () {
+  menuMobile.classList.remove("ativo");
+};
+
+document.onclick = function (evento) {
+  if (
+    !btnMobile.contains(evento.target) &&
+    !menuMobile.contains(evento.target)
+  ) {
+    menuMobile.classList.remove("ativo");
+  }
+};
